5d4cff6b314918eb1d02ecc042ea9dd8de42342b
ErgoTechKG <83173948+ErgoTechKG@users.noreply.github.com>
2023-12-13 13:02:12 +0600

    Update jwt.config.js

--- isomorphic/shared/config/jwt.config.js
+++ isomorphic/shared/config/jwt.config.js
@@ -1,10 +1,10 @@
-export default {
-  enabled: false,
-  fetchUrl: "http://localhost:9000/",
-  fetchUrlSecret: "http://localhost:9000/api/",
-  uploadUrl: "http://localhost:9000/upload/",
-  secretKey: "secretKey"
-};
+// export default {
+//   enabled: false,
+//   fetchUrl: "http://localhost:9000/",
+//   fetchUrlSecret: "http://localhost:9000/api/",
+//   uploadUrl: "http://localhost:9000/upload/",
+//   secretKey: "secretKey"
+// };
 
 
 // export default {
@@ -14,3 +14,14 @@ export default {
 //   uploadUrl: "https://kent-server.onrender.com/upload/",
 //   secretKey: "secretKey"
 // };
+
+export default {
+  enabled: false,
+  fetchUrl: "https://kent.onrender.com/",
+  fetchUrlSecret: "https://kent.onrender.com/api/",
+  uploadUrl: "https://kent.onrender.com/upload/",
+  secretKey: "secretKey"
+};
+
+
+
